ffc4e935ec7301a8e9012c4ee41268649aea34a4
Emmett Underhill <emmettu@gmail.com>
2015-08-10 20:46:40 -0400

    Adding the king piece

---
+++ src/com/emmett/chess/main/model/pieces/King.java
@@ -0,0 +1,64 @@
+package com.emmett.chess.main.model.pieces;
+
+import com.emmett.chess.main.model.Board;
+import com.emmett.chess.main.model.Position;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by emmett on 10/08/15.
+ */
+public class King extends Piece {
+    King(Board board, int color) {
+        super(board, color);
+    }
+
+    @Override
+    protected List<Position> generatePossiblePositions() {
+        List<Position> positions = new ArrayList<Position>();
+        for(Position p : getPotentialPositions()) {
+            if(!board.friendlyOccupied(p, color) && board.contains(p)) {
+                positions.add(p);
+            }
+        }
+        return positions;
+    }
+
+    private Position[] getPotentialPositions() {
+        return new Position[] {
+               new Position(
+                       currentPosition.x + 1,
+                       currentPosition.y + 1
+               ),
+                new Position(
+                       currentPosition.x - 1,
+                       currentPosition.y + 1
+               ),
+                new Position(
+                       currentPosition.x + 1,
+                       currentPosition.y - 1
+               ),
+                new Position(
+                       currentPosition.x - 1,
+                       currentPosition.y - 1
+               ),
+                new Position(
+                       currentPosition.x + 1,
+                       currentPosition.y + 1
+               ),
+                new Position(
+                       currentPosition.x - 1,
+                       currentPosition.y + 1
+               ),
+                new Position(
+                       currentPosition.x + 1,
+                       currentPosition.y - 1
+               ),
+                new Position(
+                       currentPosition.x - 1,
+                       currentPosition.y - 1
+               ),
+        };
+    }
+}
---
+++ src/com/emmett/chess/main/view/View.java
@@ -0,0 +1,7 @@
+package com.emmett.chess.main.view;
+
+/**
+ * Created by emmett on 10/08/15.
+ */
+public class View {
+}

47fb1f8fac0b985dd516dc3662c714f45876b6a4
Emmett Underhill <emmettu@gmail.com>
2015-08-11 20:09:56 -0400

    Adding board renderer to main method.

--- src/com/emmett/chess/main/Main.java
+++ src/com/emmett/chess/main/Main.java
@@ -1,7 +1,13 @@
 package com.emmett.chess.main;
 
+import com.emmett.chess.main.model.boards.ChessBoard;
+import com.emmett.chess.main.view.ConsoleBoardRenderer;
+
 public class Main {
 
     public static void main(String[] args) {
+        ConsoleBoardRenderer renderer = new ConsoleBoardRenderer(new ChessBoard());
+        renderer.update();
+        renderer.render();
     }
 }
